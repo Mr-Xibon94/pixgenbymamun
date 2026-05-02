f8f39dc1ea8b19a7d9f495fcb92252a5ebda9ce3
Mr-Xibon94 <md19jibon94@gmail.com>
2026-05-02 11:34:51 +0600

    fixed some bug

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,9 +6,12 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("pixGen");
 
 export const auth = betterAuth({
-    database: mongodbAdapter(db,{client}),
+    
+    database: mongodbAdapter(db, { client }),
     emailAndPassword: {
         enabled: true,
     }
+
+
     //...
 });
